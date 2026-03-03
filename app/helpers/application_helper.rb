36e203e65a21604d316e0d76fc455c1082eec1b1
Maxi Khevenhüller <maxi.khevenhueller@gmail.com>
2026-03-03 15:17:23 +0100

    Show Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def markdown(text)
+    return "" if text.blank?
+
+    options = { filter_html: true, hard_wrap: true, link_attributes: { rel: "nofollow", target: "_blank" } }
+    extensions = { autolink: true, superscript: true, fenced_code_blocks: true }
+    renderer = Redcarpet::Render::HTML.new(options)
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
