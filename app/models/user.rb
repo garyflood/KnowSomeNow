b25bc5507c04df88d2b7df2767b63bfe2190d51a
Gary Flood <45420768+garyflood@users.noreply.github.com>
2026-03-06 12:16:08 +0100

    password requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :devices, dependent: :destroy
+
+  # Add this custom validation for password complexity
+  validate :password_complexity, if: -> { password.present? }
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    requirements = {
+      "at least 8 characters" => password.length >= 8,
+      "at least one uppercase letter" => password.match?(/[A-Z]/),
+      "at least one lowercase letter" => password.match?(/[a-z]/),
+      "at least one number" => password.match?(/\d/),
+      "at least one special character (!@#$%^&*)" => password.match?(/[\W_]/)
+    }
+
+    missing_requirements = requirements.select { |_, met| !met }.keys
+
+    return unless missing_requirements.any?
+
+    errors.add(:password, "must include #{missing_requirements.join(', ')}")
+  end
 end
